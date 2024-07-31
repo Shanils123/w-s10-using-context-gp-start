01f25a9567d70be3ef2ad54e1f069cea733c24c0
Shanil Sanchez <shanils857@gmail.com>
2024-07-31 17:58:03 -0400

    UseContext API completed

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { TodosContext } from '../context/todos'
 
 const StyledTodo = styled.li`
   text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
@@ -11,8 +12,8 @@ export default function Todo(props) {
     todos,
     toggleTodo,
     showCompletedTodos,
-    toggleShowCompletedTodos
-  } = props
+    toggleShowCompletedTodos,
+  } = useContext(TodosContext)
 
   return (
     <div id="todos">
